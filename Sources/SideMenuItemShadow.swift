cbb2db534508d691f9ffe75e4ff7a8c3084faced
Eric Miller <eric@handsome.is>
2018-02-08 14:21:43 -0600

    Add public initializer for SideMenuItemShadow

--- Sources/SideMenuItemShadow.swift
+++ Sources/SideMenuItemShadow.swift
@@ -12,7 +12,24 @@ import Foundation
  Defines the parameters for the shadow that is shown under to content controller when the side menu is open.
 */
 public struct SideMenuItemShadow {
-    public var color: UIColor? = UIColor.black
-    public var opacity: CGFloat = 0.3
-    public var offset: CGSize = CGSize(width: -5, height: 5)
+
+    struct Defaults {
+        static var defaultColor: UIColor = UIColor.black
+        static var defaultOpacity: CGFloat = 0.3
+        static var defaultSize: CGSize = CGSize(width: -5, height: 5)
+    }
+
+    public var color: UIColor? = Defaults.defaultColor
+    public var opacity: CGFloat = Defaults.defaultOpacity
+    public var offset: CGSize = Defaults.defaultSize
+
+    public init() { }
+
+    public init(color: UIColor? = Defaults.defaultColor,
+                opacity: CGFloat = Defaults.defaultOpacity,
+                offset: CGSize = Defaults.defaultSize) {
+        self.color = color
+        self.opacity = opacity
+        self.offset = offset
+    }
 }
